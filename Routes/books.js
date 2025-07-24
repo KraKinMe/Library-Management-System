fff66f856e7595c7df6d3ea6aea0026bf5a788cb
KraKinMe <dhruvagrawal13021307@gmail.com>
2025-07-24 14:37:59 +0530

    Added new Routes for Books

--- Routes/books.js
+++ Routes/books.js
@@ -9,9 +9,59 @@ router.get('/',(req,res)=>{
     })
 })
 
-// router.get('/:id',(req,res)=>{
-//     const {id}
-// })
+router.get('/:id',(req,res)=>{
+    const {id}=req.params;
+    const thisBook=books.find((each)=>each.id===id);
+    if(!thisBook){
+        return res.status(404).json({
+            success:false,
+            message:`No book with ID: ${id}`
+        });
+    }
+    res.status(200).json({
+        success:true,
+        data:thisBook
+    })
+})
+
+
+router.post('/',(req,res)=>{
+    // "id": "3",
+    //   "name": "The Land Before Time",
+    //   "author": "Alan Moore",
+    //   "genre": "Children's",
+    //   "price": "8.50",
+    //   "publisher"
+    const {id,name,author,genre,price,publisher}=req.body;
+    
+    
+    if(!id || !name || !author || !genre || !price || !publisher){
+        return res.status(400).json({
+            success:false,
+            message:"Please provide all the details"
+        })
+    }
+    
+    const thisBook=books.find((each)=>each.id===id);
+
+    if(thisBook){
+        return res.status(409).json({
+            success:false,
+            message:`Book by ID:${id} already exists`
+        })
+    }
+
+    let newBook={id,name,author,genre,price,publisher}
+
+    books.push(newBook);
+
+    res.status(201).json({
+        success:true,
+        message:`Book added successfully`
+    })
+    
+})
 
 
 module.exports=router;
+//
